d57b0715c4de14128603228880c30a9f4b1934b2
RooneyMcNibNug <Jakea350@protonmail.com>
2021-12-05 12:21:04 -0600

    Day3 pt.1 complete & Day4 setup

--- Day 3/Day3p1.py
+++ Day 3/Day3p1.py
@@ -1,4 +1,13 @@
 from collections import Counter
 
-string = "hello world"
-print ''.join(char[0] for char in Counter(string).most_common())
+#def get_file():
+#    with open("input.txt", "r") as f:
+#        return [(c.split()[0], int(c.split()[1])) for c in f.read().splitlines()]
+
+with open("input.txt") as input:
+    m = [a.strip() for a in input.readlines()]
+
+gamma   = int("".join([Counter([x[i] for x in m]).most_common(1)[0][0] for i in range(len(m[0]))]) ,2) #most
+epsilon = int("".join([Counter([x[i] for x in m]).most_common()[-1][0] for i in range(len(m[0]))]), 2) #least
+
+print(f"Solution [gamma * epsilon]: {gamma * epsilon}")
